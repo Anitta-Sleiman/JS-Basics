f2a7460198abafcff25388721d96d4c09c6044be
anitta-sleiman <sleiman3198@gmail.com>
2023-08-30 20:08:15 +0300

    Step_7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,7 @@
+let shoeSize = prompt("Enter your Shoe-Size");
+let birthYear = prompt("Enter your Year of Birth");
+function mathEquation() {
+    var result = ((shoeSize * 2 + 5) * 50) - birthYear + 1776;
+    return result;
+}
+alert(mathEquation());
