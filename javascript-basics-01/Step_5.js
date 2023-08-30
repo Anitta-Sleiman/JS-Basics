2907e32ed325382deeeff62a2616b2ff0273f03c
anitta-sleiman <sleiman3198@gmail.com>
2023-08-30 19:47:34 +0300

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,5 @@
+var num1 = prompt("Enter your first number");
+var num2 = prompt("Enter your second number");
+var num3 = num1 * num2;
+
+alert("The result of multiplying these numbers is: " + num3)
