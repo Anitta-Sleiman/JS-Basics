262e1095dff9f24568122b670247386a386f9a2e
anitta-sleiman <sleiman3198@gmail.com>
2023-08-30 19:41:10 +0300

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+let userName = prompt("Enter your name");
+let userSurname = prompt("Enter your surname");
+let userCity = prompt("Enter your city");
+alert("Hello " + userName + " " + userSurname + " from " + userCity);
