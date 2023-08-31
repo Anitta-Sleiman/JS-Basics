2bc620d0d600310cd1ab7d27768be8d4a3497d0f
anitta-sleiman <sleiman3198@gmail.com>
2023-08-31 05:03:33 +0300

    step 5

--- javascript-basics-02/step 5/step 5.js
+++ javascript-basics-02/step 5/step 5.js
@@ -1,8 +1,8 @@
+const image = document.querySelector(`#image${i}`);
 for (let i = 1; i <= 5; i++) {
-    const image = document.querySelector(`#image${i}`);
-  
-    image.addEventListener("mouseover", function () {
-      image.src = `images/image${i}_hover.jpg`;
+    image.addEventListener("mouseover", function (event) {
+      event.target.src = `images/image${i}_hover.jpg`;
     });
+
   }
   
