6a04c3ba79b0304e76f43f316ee674a4f2d3bbce
anitta-sleiman <sleiman3198@gmail.com>
2023-08-31 05:12:30 +0300

    step 6

--- javascript-basics-02/step 6/step 6.js
+++ javascript-basics-02/step 6/step 6.js
@@ -1,11 +1,11 @@
 for (let i = 1; i <= 5; i++) {
     const image = document.querySelector(`#image${i}`);
 
-    image.addEventListener("mouseover", function () {
-        image.src = `images/image${i}_hover.jpg`;
+    image.addEventListener("mouseover", function (event) {
+        event.target.src = `images/image${i}_hover.jpg`;
     });
 
-    image.addEventListener("mouseout", function () {
-        image.src = `images/image${i}.jpg`;
+    image.addEventListener("mouseout", function (event) {
+        event.target.src = `images/image${i}.jpg`;
     });
 }
