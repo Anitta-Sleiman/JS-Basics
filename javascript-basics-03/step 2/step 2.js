3c1d38cbaf6433a4bdc60abfcbcec95d2567fd51
anitta-sleiman <sleiman3198@gmail.com>
2023-09-01 20:06:24 +0300

    step 2

--- javascript-basics-03/step 2/step 2.js
+++ javascript-basics-03/step 2/step 2.js
@@ -1,14 +1,9 @@
 const showLink = document.getElementById("show");
 const hideLink = document.getElementById("hide");
 const jsText = document.getElementById("texte");
-
-
 showLink.addEventListener("click", function () {
     jsText.style.display = "block";
 });
 hideLink.addEventListener("click", function () {
     jsText.style.display = "none";
 });
-
-
-
