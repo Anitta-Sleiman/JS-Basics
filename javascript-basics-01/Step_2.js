c5d46927241d81c65ea6e93138a4725276c04dc9
anitta-sleiman <sleiman3198@gmail.com>
2023-09-01 19:20:35 +0300

    Step_2

--- javascript-basics-01/Step_2.js
+++ javascript-basics-01/Step_2.js
@@ -2,5 +2,5 @@ let userName = "Doe";
 let userSurname = "John";
 let userCity = "New-York";
 
-let fullSentence = "Name: " + userName + "\n Surname: " + userSurname + "\n City: " + userCity;
+let fullSentence = "Name: " + userName + "\nSurname: " + userSurname + "\nCity: " + userCity;
 alert(fullSentence);
